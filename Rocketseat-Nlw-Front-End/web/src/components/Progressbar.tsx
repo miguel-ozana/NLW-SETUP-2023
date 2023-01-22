25be86974aa5287e45af2582f8aa7278b782db57
Miguel Ozana <miguelozana74@gmail.com>
2023-01-22 14:47:59 -0300

    Initial commit

---
+++ Rocketseat-Nlw-Front-End/web/src/components/Progressbar.tsx
@@ -0,0 +1,20 @@
+interface ProgBarProps{
+  progress: number
+}
+
+export function Progressbar(props: ProgBarProps){
+  const progressStyles ={
+    width: `${props.progress}%`
+  }
+  return(
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+    <div 
+      role="progressbar"
+      aria-label="Progresso de hábitos completados nesse dia"
+      aria-valuenow={props.progress}
+      className="h-3 rounded-xl bg-violet-600 transition-all"
+      style={{ width: `${props.progress}%`}}
+    />
+  </div>
+  )
+}
